8930b6aeafc318aa43fd501207494971dc15a986
hello_world <86287727+hello-world-was-taken@users.noreply.github.com>
2021-12-16 11:49:42 +0000

    Create Domino_piling.py

---
+++ week_1/Domino_piling.py
@@ -0,0 +1,13 @@
+#QUESTION
+#https://codeforces.com/problemset/problem/50/A
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int row, column;
+    cin >> row >> column;
+    
+    cout << (row * column) / 2;
+}
