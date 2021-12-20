2bbccaed7e4c0bab8abeaff69bdb0d493c3b98a7
hello_world <86287727+hello-world-was-taken@users.noreply.github.com>
2021-12-20 12:52:52 +0000

    Update maximum-number-of-coins-you-can-get.py

--- week_2/maximum-number-of-coins-you-can-get.py
+++ week_2/maximum-number-of-coins-you-can-get.py
@@ -6,17 +6,13 @@ class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         piles.sort()
         arr_len = len(piles)
-        chosen = []
         count = 0
+        ans = 0
         for i in range(arr_len):
-            chosen.append([piles[i], piles[-2-(i*2)], piles[-1-(i*2)]])
+            ans += piles[-2-(i*2)]
             count += 1
             if count == arr_len // 3:
                 break
             else: 
                 continue
-        ans = 0
-        print(chosen)
-        for i in chosen:
-            ans += i[1]
         return ans
