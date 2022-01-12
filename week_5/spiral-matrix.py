e72f725080066c351a55a395bc993d426e7bac4d
hello_world <86287727+hello-world-was-taken@users.noreply.github.com>
2022-01-12 17:51:00 +0300

    Update spiral-matrix.py

--- week_5/spiral-matrix.py
+++ week_5/spiral-matrix.py
@@ -38,3 +38,66 @@ class Solution:
                 isRight = True
                 isup = False
         return ans
+    
+    
+    # More efficient solution to the question using 2 variables and a pointer.
+    class Solution:     
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        ans = []
+        isRight = True
+        isDown = False
+        isUp = False
+        isLeft = False
+        
+        curr_index = [0,-1]
+        row = len(matrix)
+        col = len(matrix[0])
+        while row > 0 and col > 0:
+            # print(curr_index)
+            if col > 0 and isRight:
+                for i in range(col):
+                    # print("isRight: ", curr_index, row, col)
+                    curr_index[1] += 1
+                    ans.append(matrix[curr_index[0]][curr_index[1]])
+                row -= 1
+                isRight=False
+                isDown= True
+                isUp = False
+                isLeft = False
+            
+            
+            if row > 0 and isDown:
+                for i in range(row):
+                    # print("isDown: ", curr_index, row, col)
+                    curr_index[0] += 1
+                    ans.append(matrix[curr_index[0]][curr_index[1]])
+                col -= 1
+                isDown= False
+                isLeft = True
+                isRight=False
+                isUp = False
+            
+            
+            if col > 0 and isLeft:
+                for i in range(col):
+                    # print("isLeft: ", curr_index, row, col)
+                    curr_index[1] -= 1
+                    ans.append(matrix[curr_index[0]][curr_index[1]])
+                row -= 1
+                isLeft = False
+                isUp = True
+                isDown= False
+                isRight=False
+            
+            
+            if row > 0 and isUp:
+                for i in range(row):
+                    # print("isUp: ", curr_index, row, col)
+                    curr_index[0] -= 1
+                    ans.append(matrix[curr_index[0]][curr_index[1]])
+                col -= 1
+                isRight = True
+                isup = False
+                isLeft = False
+                isDown= False
+        return ans
