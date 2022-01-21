a01e1278bdba79a29a7dffd1093d5abd7abc70de
hello_world <86287727+hello-world-was-taken@users.noreply.github.com>
2022-01-21 12:53:10 +0300

    Create power-of-three.py

---
+++ week_6/power-of-three.py
@@ -0,0 +1,17 @@
+#https://leetcode.com/problems/power-of-three/
+
+class Solution:
+    def helper(self, n):
+        if n == 1:
+            return True
+        if n%3 != 0:
+            return False
+        else:
+            return self.isPowerOfThree(n//3)
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 0:
+            return False
+        elif n == 1:
+            return True
+        else:
+            return self.helper(n)
