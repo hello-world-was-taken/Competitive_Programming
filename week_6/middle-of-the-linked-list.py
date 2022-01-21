ccfa0f7df4e7fad568ea1959f287bfb6fd4ba323
hello_world <86287727+hello-world-was-taken@users.noreply.github.com>
2022-01-21 13:01:01 +0300

    Create middle-of-the-linked-list.py

---
+++ week_6/middle-of-the-linked-list.py
@@ -0,0 +1,24 @@
+#https://leetcode.com/problems/middle-of-the-linked-list/
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        turtle = head
+        hare = head
+        
+        while turtle and turtle.next:
+            hare = hare.next
+            turtle = turtle.next.next
+        return hare
+#         count = []
+#         while head != None:
+#             count.append(head)
+#             head = head.next
+        
+#         return count[len(count)//2]
+        
