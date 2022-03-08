ad8ad578768b411032cda9b6b1208c827e3923fa
hello_world <86287727+hello-world-was-taken@users.noreply.github.com>
2022-03-08 14:49:08 +0300

    Create boats-to-save-people.py

---
+++ week_12/boats-to-save-people.py
@@ -0,0 +1,25 @@
+#https://leetcode.com/problems/boats-to-save-people/
+
+
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        i = 0
+        j = len(people) - 1
+        boat_count = 0
+        
+        # consider the case when there is only one person
+        if len(people) == 1:
+            return 1
+        while i <= j:
+            if people[i] + people[j] <= limit:
+                boat_count += 1
+                i += 1
+                j -= 1
+            else:
+                boat_count += 1
+                j -= 1
+                
+        return boat_count
+        
