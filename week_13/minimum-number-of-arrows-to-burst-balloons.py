615dfbb41d2a7721ff4b6ced1381cce46de5739f
hello_world <86287727+hello-world-was-taken@users.noreply.github.com>
2022-04-04 13:16:42 +0300

    Create minimum-number-of-arrows-to-burst-balloons.py

---
+++ week_13/minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x: x[1])
+        
+        # do it for the first round
+        just_popped = points[0][1]
+        arrows_fired = 1
+        for i in range(1, len(points)):
+            if points[i][0] <= just_popped <= points[i][1]:
+                pass
+            else:
+                just_popped = points[i][1]
+                arrows_fired += 1
+        return arrows_fired
