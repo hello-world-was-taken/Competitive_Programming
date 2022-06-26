26ff432b4ed991f443830bc5c7443e31356ca187
hello_world <86287727+hello-world-was-taken@users.noreply.github.com>
2022-06-26 19:30:36 +0300

    Create single-number.py

---
+++ Inactive_season/single-number.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/single-number/
+
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for i in nums:
+            ans ^= i
+        return ans
