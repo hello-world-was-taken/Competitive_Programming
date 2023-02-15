a1f8e7e87a8ed2387ae5e01cc5d03657ddbe849c
hello_world <86287727+hello-world-was-taken@users.noreply.github.com>
2023-02-15 21:45:32 +0300

    Create peeking-iterator.py

---
+++ Squid_Game/peeking-iterator.py
@@ -0,0 +1,68 @@
+# https://leetcode.com/problems/peeking-iterator/
+
+
+# Below is the interface for Iterator, which is already defined for you.
+#
+# class Iterator:
+#     def __init__(self, nums):
+#         """
+#         Initializes an iterator object to the beginning of a list.
+#         :type nums: List[int]
+#         """
+#
+#     def hasNext(self):
+#         """
+#         Returns true if the iteration has more elements.
+#         :rtype: bool
+#         """
+#
+#     def next(self):
+#         """
+#         Returns the next element in the iteration.
+#         :rtype: int
+#         """
+
+class PeekingIterator:
+    def __init__(self, iterator):
+        """
+        Initialize your data structure here.
+        :type iterator: Iterator
+        """
+        self.iterator = iterator
+        self._next = None
+        if self.iterator.hasNext():
+            self._next = self.iterator.next()
+        
+
+    def peek(self):
+        """
+        Returns the next element in the iteration without advancing the iterator.
+        :rtype: int
+        """
+        return self._next
+        
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        ans = self._next
+        if self.iterator.hasNext():
+            self._next = self.iterator.next()
+        else:
+            self._next = None
+        
+        return ans
+    
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return self._next != None
+        
+
+# Your PeekingIterator object will be instantiated and called as such:
+# iter = PeekingIterator(Iterator(nums))
+# while iter.hasNext():
+#     val = iter.peek()   # Get the next element but not advance the iterator.
+#     iter.next()         # Should return the same value as [val].
